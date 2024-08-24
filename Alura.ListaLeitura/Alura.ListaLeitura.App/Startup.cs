ec1d7f1cbf6aec143f6014dbc6724039236d7f0c
AlanGustav0 <alagustavo946@gmail.com>
2024-08-24 14:51:22 -0300

    feature: Working with requests

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,7 +10,30 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var repository = new LivroRepositorioCSV();
+
+            var validPaths = new Dictionary<string, RequestDelegate>()
+            {
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos },
+            };
+
+            if(validPaths.ContainsKey(context.Request.Path))
+            {
+                var method = validPaths[context.Request.Path];
+                return method.Invoke(context);
+            }
+
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho não encontrado!");
+
         }
 
         public Task LivrosParaLer(HttpContext context)
@@ -17,6 +41,20 @@ namespace Alura.ListaLeitura.App
             var repository = new LivroRepositorioCSV();
             return context.Response.WriteAsync(repository.ParaLer.ToString());
  
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var repository = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(repository.Lendo.ToString());
+
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var repository = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(repository.Lidos.ToString());
+
         }
     }
 }
