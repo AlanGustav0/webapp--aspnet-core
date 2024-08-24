3fb00fff4767a36e45070b13d546e5443d34c7e8
AlanGustav0 <alagustavo946@gmail.com>
2024-08-24 08:48:28 -0300

    feature: Implements WebHost and Startup class on application

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(LivrosParaLer);
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var repository = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(repository.ParaLer.ToString());
+ 
+        }
+    }
+}
